5c71c6acacf27105e383b56895455fb5b0ef99c2
ChaouachiYasmine <chaouachiyasmine28@gmail.com>
2022-11-23 16:12:58 +0100

    add slider branch & list component

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import List from '../component/list/List'
 import "./Home.css"
 const Home = () => {
     return (
         <div className='home' >
+            <List />
+            <List />
+            <List />
+            <List />
+            <List />
+            <List />
+            <List />
+            <List />
 
         </div>
     )
