b78e09a876bfe9e93d5cd9b521a0be475905ef4b
Hejer Rhouma <rhoumahejer20@gmail.com>
2022-11-23 16:18:28 +0100

    adding a logo

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/navbar/Navbar'
 import "./Home.css"
 const Home = () => {
     return (
         <div className='home' >
+            <Navbar/>
 
         </div>
     )
