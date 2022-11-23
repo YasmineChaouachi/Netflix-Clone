b96d2e2e0bb8bcd5e4c284762fcac9617f29ab12
Hejer Rhouma <rhoumahejer20@gmail.com>
2022-11-23 18:20:18 +0100

    add css to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,26 +1,45 @@
-import { Notifications, Search } from '@mui/icons-material'
+import { ArrowDropDown, Notifications, Search } from '@mui/icons-material'
 import React from 'react'
 import "./navbar.css"
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
   return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
     <div className="navbar">
-        <div className="container">
-          <div className="left">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt=" "/>
-            <span>Homepage</span>
+      <div className="container">
+        <div className="left">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt=" " />
+          <span>Homepage</span>
           <span>Series</span>
           <span>Movies</span>
           <span>New and Popular</span>
           <span>My List</span>
-          </div>
-          <div className="right">
-            <Search/>
-            <Notifications/>
-            
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <Notifications className="icon" />
+          <img src="http://occ-0-1007-3996.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABZ2iWyDq0fR9TY_uztNZ4TcwWkPPdS2NdTtUt3EHjC_rkiEAexSxSUfbrAYTaiI5pcHVs5QMIAhgo1tVaDJr67VjRcr_ZCw.png?r=cad" alt="" />
+          <div className="profile">
+
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+
           </div>
         </div>
-        </div>
+
+      </div>
+    </div>
+    </div>
   )
 }
 
