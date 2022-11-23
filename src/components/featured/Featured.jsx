b2eaedf050d6c20a1aadf51fa35a5dce794abc53
Hejer Rhouma <rhoumahejer20@gmail.com>
2022-11-23 21:21:57 +0100

    add an image

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div className="featured">
+        <img src="https://theentertainmentpit.co.uk/wp-content/uploads/2020/12/Joker.jpg" alt="" />
+
+    </div>
+  )
+}
+
+export default Featured
