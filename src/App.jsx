509522e0f173fa15dde4dcf23cb54739bf8e9d04
Hejer Rhouma <rhoumahejer20@gmail.com>
2022-11-24 18:54:55 +0100

    adding the form to login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import Home from "./pages/home/Home";
 import "./App.css";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Home />;
+  return <Login />;
 };
 
 export default App;
