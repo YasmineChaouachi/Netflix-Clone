4b59085b81775f48786329620d7ccdc4cfe544b0
Hejer Rhouma <rhoumahejer20@gmail.com>
2022-11-24 15:20:08 +0100

    adding a handleStart and handleFinish functions

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./register.css"
 
 const Register = () => {
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    const handleStart = ()=>{
+        setEmail(emailRef.current.value);
+    };
+    const handleFinish = ()=>{
+        setPassword(passwordRef.current.value);
+    };
     return (
         <div className="register">
             <div className="top">
-<div className="wrapper">
-
-
-                <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png " alt="" />
-                <button className="loginButton">Sign In</button>
-            </div>
+                <div className="wrapper">
+                    <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png " alt="" />
+                    <button className="loginButton">Sign In</button>
+                </div>
             </div>
             <div className="container">
                 <h1>Unlimited movies, TV shows, and more.</h1>
@@ -18,10 +27,17 @@ const Register = () => {
                 <p>
                     Ready to watch? Enter your email to create or restart your membership.
                 </p>
-                <div className="input">
-                    <input type="email" placeholder="email address" />
-                    <button className="registerButton">Get started</button>
-                </div>
+                {!email ? (
+                        <div className="input">
+                        <input type="email" placeholder="email address" ref={emailRef} />
+                        <button className="registerButton" onClick={handleStart}>Get started</button>
+                    </div>
+
+                    ) :  (  <form className="input">
+                    <input type="password" placeholder="password" ref={passwordRef} />
+                    <button className="registerButton" onClick={handleFinish}>Start</button>
+                </form> )}
+              
             </div>
 
         </div>
