6a351fae5fabf4677542ad352c5c2617383a79ce
Hejer Rhouma <rhoumahejer20@gmail.com>
2022-11-24 14:17:47 +0100

    add a login button

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import "./register.css"
 
 const Register = () => {
-  return (
-    <div className="register">
-        <div className="top">
-            <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png " alt="" />
-        </div>
+    return (
+        <div className="register">
+            <div className="top">
 
-    </div>
-  )
+                <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png " alt="" />
+                <button className="loginButton"></button>
+            </div>
+
+        </div>
+    )
 }
 
 export default Register
